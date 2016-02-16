463f5af428edf1ff391026b3c6fcac71283e5e3e
rakesh55555 <sanu.rakesh94@gmail.com>
2016-02-16 15:01:36 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,4 +1,4 @@
 <?php
-$con = mysql_connect("localhost","root","123") or die("error to connect");
+$con = mysql_connect("localhost","adminIHNwYY8","123") or die("error to connect");
 	mysql_select_db("interview_pro_data",$con);
 ?>
