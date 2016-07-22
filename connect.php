5ad1dd13dc6b5f4396d4097e03015e9850dd9cc0
rakesh55555 <sanu.rakesh94@gmail.com>
2016-07-22 16:05:44 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,4 +1,4 @@
 <?php
-$con = mysql_connect("php-interviewpro.rhcloud.com","adminXk8s4c8","123") or die("ERror to connect");
+$con = mysql_connect("127.9.88.2","adminXk8s4c8","123") or die("ERror to connect");
 	mysql_select_db("php",$con);
 ?>
